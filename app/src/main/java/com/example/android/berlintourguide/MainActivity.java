a9dc191c1daa6cd84a5bb7491cb3968627576c2c
Anas <anas.marrawi@gmail.com>
2017-04-14 13:09:53 +0200

    Modify the way the ViewPager and TabLayout is displayed (with a better code)

--- app/src/main/java/com/example/android/berlintourguide/MainActivity.java
+++ app/src/main/java/com/example/android/berlintourguide/MainActivity.java
@@ -1,7 +1,11 @@
 package com.example.android.berlintourguide;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.design.widget.TabLayout;
+import android.support.v4.view.ViewPager;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +13,20 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // Get the toolbar
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        // Get the ViewPager and set it's PagerAdapter so that it can display items
+        ViewPager viewPager = (ViewPager) findViewById(R.id.view_pager);
+        viewPager.setAdapter(new PagerAdapter(getSupportFragmentManager(),
+                MainActivity.this));
+
+        // Give the TabLayout the ViewPager
+        TabLayout tabLayout = (TabLayout) findViewById(R.id.tab_layout);
+        tabLayout.setupWithViewPager(viewPager);
     }
+
+
 }
--- app/src/main/java/com/example/android/berlintourguide/PagerAdapter.java
+++ app/src/main/java/com/example/android/berlintourguide/PagerAdapter.java
@@ -1,14 +1,19 @@
 package com.example.android.berlintourguide;
 
+import android.content.Context;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentPagerAdapter;
 
 
 public class PagerAdapter extends FragmentPagerAdapter {
+    final int PAGE_COUNT = 4;
+    private String tabTitles[] = new String[]{"Attractions", "Events", "Restaurants", "Hotels"};
+    private Context context;
 
-    public PagerAdapter(FragmentManager fm) {
+    public PagerAdapter(FragmentManager fm, Context context) {
         super(fm);
+        this.context = context;
     }
 
     @Override
@@ -25,7 +30,13 @@ public class PagerAdapter extends FragmentPagerAdapter {
     }
 
     @Override
+    public CharSequence getPageTitle(int position) {
+        // Generate title based on item position
+        return tabTitles[position];
+    }
+
+    @Override
     public int getCount() {
-        return 4;
+        return PAGE_COUNT;
     }
 }
